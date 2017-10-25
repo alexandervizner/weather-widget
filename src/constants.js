c9e2d8191df0d64cb77e9db157e5d3c1582a1a32
alexandervizner <33081943+alexandervizner@users.noreply.github.com>
2017-10-25 09:41:23 +0300

    Removing API KEY value

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const BASE_API_URL = "http://api.openweathermap.org/data/2.5/";
-const KEY = "0bd3f0724af19f30b047bf8372bcdc98";
+const KEY = "";
 const DEFAULT_CITIES = [524901, 588409, 2643743, 2950159];
 
 export {
